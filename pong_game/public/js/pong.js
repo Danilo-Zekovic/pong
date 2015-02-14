61a2e4303156283572950aaac84beebd8223df14
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-02-14 13:03:06 -0500

    Added basic code

--- pong_game/public/js/pong.js
+++ pong_game/public/js/pong.js
@@ -1,5 +1,21 @@
 /*
  * pong.js
+ * Root namespace module
  */
 
+/*jslint           browser : true,   continue : true,
+  devel  : true,    indent : 2,       maxerr  : 50,
+  newcap : true,     nomen : true,   plusplus : true,
+  regexp : true,    sloppy : true,       vars : false,
+  white  : true
+*/
+/*global $, pong */
+
+var pong = (function () {
+  var initModule = function ( $container ) {
+    pong.shell.initModule( $container );
+  }
+
+  return {initModule: initModule};
+}());
 
