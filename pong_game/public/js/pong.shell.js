4584ce71acac49a3ba1badf8b6cd690bd7b85cf4
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-06 18:17:10 -0500

    Added some constants and the player object

--- pong_game/public/js/pong.shell.js
+++ pong_game/public/js/pong.shell.js
@@ -15,11 +15,45 @@
   var
     configMap = {
       main_html : String()
-	+ '<p>Welcome to Pong Game</p>'
+	+ '<p>Welcome to Pong Game2</p>'
     },
     stateMap = { $container : null },
     jqueryMap = {},
+    // constants for the canvas size
+    WIDTH = 700, HEIGHT = 600,
+
+    pi = Math.PI,
+
+    UpArrow = 38, DownArrow = 40,
     setJqueryMap, initModule;
+
+    // Game elements
+    var
+      canvas,
+      ctx,
+      keystate;
+
+    // The player paddle
+    // type Object
+    player = {
+      x: null,
+      y: null,
+
+      width:  20,
+      height: 100,
+      // Update the position depending on pressed keys
+      update: function() {
+	if (keystate[UpArrow]) this.y -= 7;
+          if (keystate[DownArrow]) this.y += 7;
+	    // keep the paddle inside of the canvas
+	    this.y = Math.max(Math.min(this.y, HEIGHT - this.height), 0);
+      },
+    // Draw the player paddle to the canvas
+      draw: function() {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
+
   //------------------END MODULE SCOPE VAR----------------
 
   //------------------BEGIN UTILITY METHODS---------------
