32f319359c0ca1a7cd6e3930273e9993fc1bda82
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-06 21:37:29 -0500

    Added ai object, that is automated paddel

--- pong_game/public/js/pong.shell.js
+++ pong_game/public/js/pong.shell.js
@@ -31,7 +31,7 @@
   var
     canvas,
     ctx,
-    keystate;
+    keystate,
 
   // The player paddle
   // type Object
@@ -52,7 +52,27 @@
     draw: function() {
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-  }
+  },
+
+  ai = {
+    x: null,
+    y: null,
+    width:  20,
+    height: 100,
+    // Update the position depending on the ball position
+    update: function() {
+      // calculate ideal position
+      var desty = ball.y - (this.height - ball.side)*0.5;
+      // ease the movement towards the ideal position
+      this.y += (desty - this.y) * 0.1;
+      // keep the paddle inside of the canvas
+      this.y = Math.max(Math.min(this.y, HEIGHT - this.height), 0);
+    },
+    // Draw the ai paddle to the canvas
+    draw: function() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  },
 
   //------------------END MODULE SCOPE VAR----------------
 
