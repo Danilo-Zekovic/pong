9fcfcc3d3f8213408c49be5059cf6340fa7db47e
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-07 19:04:35 -0500

    Added the ball object

--- pong_game/public/js/pong.shell.js
+++ pong_game/public/js/pong.shell.js
@@ -74,6 +74,78 @@
     }
   },
 
+
+  ball = {
+    x:   null,
+    y:   null,
+    vel: null,
+    side:  20,
+    speed: 12,
+    // Serves the ball towards the specified side
+    // @param  {number} side 1 right
+    //                      -1 left
+    serve: function(side) {
+      // set the x and y position
+      var r = Math.random();
+      this.x = side===1 ? player.x+player.width : ai.x - this.side;
+      this.y = (HEIGHT - this.side)*r;
+      // calculate out-angle, higher/lower on the y-axis =>
+      // steeper angle
+      var phi = 0.1*pi*(1 - 2*r);
+      // set velocity direction and magnitude
+      this.vel = {
+	x: side*this.speed*Math.cos(phi),
+	y: this.speed*Math.sin(phi)
+      }
+    },
+    
+
+    update: function() {
+      // update position with current velocity
+      this.x += this.vel.x;
+      this.y += this.vel.y;
+      // check if out of the canvas in the y direction
+      if (0 > this.y || this.y+this.side > HEIGHT) {
+	// calculate and add the right offset, i.e. how far
+	// inside of the canvas the ball is
+	var offset = this.vel.y < 0 ? 0 - this.y : HEIGHT - (this.y+this.side);
+	this.y += 2*offset;
+        // mirror the y velocity
+	this.vel.y *= -1;
+      }
+      // helper function to check intesectiont between two
+      // axis aligned bounding boxex (AABB)
+      var AABBIntersect = function(ax, ay, aw, ah, bx, by, bw, bh){
+        return ax < bx+bw && ay < by+bh && bx < ax+aw && by < ay+ah;
+      };
+      // check againts target paddle to check collision in x
+      // direction
+      var pdle = this.vel.x < 0 ? player : ai;
+      if (AABBIntersect(pdle.x, pdle.y, pdle.width, pdle.height,
+        this.x, this.y, this.side, this.side)
+      ) {	
+        // set the x position and calculate reflection angle
+        this.x = pdle===player ? player.x+player.width : ai.x - this.side;
+        var n = (this.y+this.side - pdle.y)/(pdle.height+this.side);
+        var phi = 0.25*pi*(2*n - 1); // pi/4 = 45
+        // calculate smash value and update velocity
+        var smash = Math.abs(phi) > 0.2*pi ? 1.5 : 1;
+        this.vel.x = smash*(pdle===player ? 1 : -1)*this.speed*Math.cos(phi);
+        this.vel.y = smash*this.speed*Math.sin(phi);
+      }
+      // reset the ball when ball outside of the canvas in the
+      // x direction
+      if (0 > this.x+this.side || this.x > WIDTH) {
+        this.serve(pdle===player ? 1 : -1);
+      }
+    },
+    // Draw the ball to the canvas
+    draw: function() {
+      ctx.fillRect(this.x, this.y, this.side, this.side);
+    }
+  };
+
+
   //------------------END MODULE SCOPE VAR----------------
 
   //------------------BEGIN UTILITY METHODS---------------
